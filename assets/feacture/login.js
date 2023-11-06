ae24ad9e51ba7d960fecc179e8c9ae4d1d15a79f
Lucas Silva <lucasyannsantossilva@hotmail.com>
2023-11-06 19:28:54 -0300

    primeiro_commit

---
+++ assets/feacture/login.js
@@ -0,0 +1,45 @@
+function verificationLogin() {
+    document.getElementById('form').addEventListener('submit', ev => {
+        ev.preventDefault()
+    })
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        let users = JSON.parse(localStorage.getItem('users')) || [];
+        console.log(users)
+        let userFound = users.find(function(user) {
+            return user.username === username && user.password === password;
+        });
+
+        if (userFound) {
+            alert('Login bem-sucedido!');
+            window.location.href = 'dashboard.html';
+        } else {
+            alert('Nome de usuário ou senha incorretos.');
+        }
+}
+
+function registerNewUser() {   
+    document.getElementById('form').addEventListener('submit', ev => {
+        ev.preventDefault()
+    })
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPass = document.getElementById("confirmPass").value
+
+    if(password === confirmPass && username !== "") {
+        let users = JSON.parse(localStorage.getItem('users')) || [];
+        users.push({ username: username, password: password });
+        localStorage.setItem('users', JSON.stringify(users));
+    
+        alert('Usuário cadastrado com sucesso!');
+    } else {
+        alert("As senhas não são iguais!")
+    }
+    console.log(users)
+}
+
+function clearStorage () {
+    localStorage.clear();
+}
